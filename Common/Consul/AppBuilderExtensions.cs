e809fe1fb57fcd7e3851392f23c01994b3d1f793
JackSu-cc <929013002@qq.com>
2020-09-20 16:28:14 +0800

    提交更改

---
+++ Common/Consul/AppBuilderExtensions.cs
@@ -0,0 +1,72 @@
+﻿
+//******************************************************************/
+// Copyright (C) 2020 All Rights Reserved.
+// CLR版本：4.0.30319.42000
+// 版本号：V1.0.0
+// 文件名：Class1
+// 创建者：名字 (cc)
+// 创建时间：2020/9/20 16:21:51
+//
+// 描述：
+//
+// 
+//=================================================================
+// 修改人：
+// 时间：
+// 修改说明：
+// 
+//******************************************************************/
+
+
+using Consul;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Consul
+{
+    public static class AppBuilderExtensions
+    {
+        public static IApplicationBuilder RegisterConsul(this IApplicationBuilder app, IApplicationLifetime lifetime, ServiceEntity serviceEntity)
+        {
+            var consulClient = new ConsulClient(x => x.Address = new Uri($"http://{serviceEntity.ConsulIP}:{serviceEntity.ConsulPort}"));//请求注册的 Consul 地址
+            var httpCheck = new AgentServiceCheck()
+            {
+                DeregisterCriticalServiceAfter = TimeSpan.FromSeconds(5),//服务启动多久后注册
+                Interval = TimeSpan.FromSeconds(10),//健康检查时间间隔，或者称为心跳间隔
+                HTTP = $"http://{serviceEntity.IP}:{serviceEntity.Port}/api/health",//健康检查地址
+                Timeout = TimeSpan.FromSeconds(5)
+            };
+
+            // Register service with consul
+            var registration = new AgentServiceRegistration()
+            {
+                Checks = new[] { httpCheck },
+                ID = Guid.NewGuid().ToString(),
+                Name = serviceEntity.ServiceName,
+                Address = serviceEntity.IP,
+                Port = serviceEntity.Port,
+                Tags = new[] { $"urlprefix-/{serviceEntity.ServiceName}" }//添加 urlprefix-/servicename 格式的 tag 标签，以便 Fabio 识别
+            };
+
+            consulClient.Agent.ServiceRegister(registration).Wait();//服务启动时注册，内部实现其实就是使用 Consul API 进行注册（HttpClient发起）
+            lifetime.ApplicationStopping.Register(() =>
+            {
+                consulClient.Agent.ServiceDeregister(registration.ID).Wait();//服务停止时取消注册
+            });
+
+            return app;
+        }
+    }
+
+    public class ServiceEntity
+    {
+        public string IP { get; set; }
+        public int Port { get; set; }
+        public string ServiceName { get; set; }
+        public string ConsulIP { get; set; }
+        public int ConsulPort { get; set; }
+    }
+}
