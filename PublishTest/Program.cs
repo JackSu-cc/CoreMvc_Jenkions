5ca7b331e75b40a1f428832fb3d9d886d7f93c74
JackSu-cc <929013002@qq.com>
2020-12-07 22:00:08 +0800

    增加RabbitMQ测试程序

---
+++ PublishTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PublishTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
